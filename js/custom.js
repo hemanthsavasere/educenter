67a6141cbe266aa253e27fd57f30090acdae0cb6
Hemanth Savasere <hemanth.savasere@gmail.com>
2017-07-26 20:55:00 +0530

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,27 +1,3 @@
-/**	
-	* Template Name: WpF Degree - Ultra Responsive Bootstrap Educational Html5 Template
-	* Version: 1.0	
-	* Template Scripts
-	* Author: WpFreeware Team
-	* Author URI: http://wpfreeware.com/
-
-	Custom JS
-	
-	1. DROPDOWN MENU
-	2. SUPERSLIDES SLIDER
-	3. NEWS SLIDER
-	4. SKILL CIRCLE
-	5. WOW SMOOTH ANIMATIN
-	6. COURSE SLIDER
-	7. TUTORS SLIDER
-	8. BOOTSTRAP TOOLTIP
-	9. PRELOADER
-	10. EVENTS SLIDER
-	11. GALLERY SLIDER
-	12. SCROLL TOP BUTTON
-	13. SCROLL UP BUTTON	 	
-	
-**/
 
 jQuery(function($){
 
